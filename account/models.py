8e764901046bcbc29fc30796663550556e96cd0e
Ben Rabinovitch <ben@jotter.ca>
2012-03-09 01:02:28 -0800

    The UserProfile model
    for storing additional info such as access_level
    resolves #2

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    access_level = models.CharField(max_length=16, default='basic',
+            choices=(
+                ('basic', 'Basic Access'),
+                ('full', 'Full Access'),
+                ))
+
+@receiver(post_save, sender=User)
+def user_profile_handler(sender, **kw):
+    if kw.get('created', False):
+        UserProfile.objects.create(user=kw.get('instance'))
+
