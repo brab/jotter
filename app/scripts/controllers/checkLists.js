b20c490304589f13bdda8153c58a437f37b2c8ee
Ben Rabinovitch <github@ben.rabinovitch.ca>
2013-11-20 16:04:50 -0800

    Some lists nav + early styling

--- app/scripts/controllers/checkLists.js
+++ app/scripts/controllers/checkLists.js
@@ -1,5 +1,13 @@
 'use strict()';
 
-jotterApp.controller('CheckListsCtrl', ['$scope', function ($scope) {
+jotterApp.controller('CheckListsCtrl',
+['$scope', 'CheckList', 'Session',
+function ($scope, CheckList, Session) {
+  Session.getUser({
+    onSuccess: function (user) {
+      $scope.user = user;
+    }
+  });
 
+  $scope.checkLists = CheckList.query();
 }]);
