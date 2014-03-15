3d6b6e4306bc8f43508187b1e06ead5b4eed81df
Ben Rabinovitch <github@ben.rabinovitch.ca>
2014-03-14 17:35:51 -0700

    Improve some syntax

--- app/scripts/controllers/checkList.js
+++ app/scripts/controllers/checkList.js
@@ -2,21 +2,21 @@
 
 jotterApp.controller('CheckListCtrl',
 ['$location', '$routeParams', '$scope', 'CheckList', 'CheckListItem', 'Session', 'User',
-function ($location, $routeParams, $scope, CheckList, CheckListItem, Session, User) {
+function($location, $routeParams, $scope, CheckList, CheckListItem, Session, User) {
   $scope.checkListItemEdit = {};
 
   Session.getUser({
-    onSuccess: function (user) {
+    onSuccess: function(user) {
       $scope.user = user;
     }
   });
 
-  User.query(function (users) {
-    $scope.users = _.reject(users, function (user) { return user.username === '!'; });
+  User.query(function(users) {
+    $scope.users = _.reject(users, function(user) { return user.username === '!'; });
   });
 
   var checkListId = $routeParams.id;
-  if (angular.isUndefined(checkListId) ||
+  if(angular.isUndefined(checkListId) ||
       checkListId === '') {
     $scope.$root.$broadcast('alert', {
       title: "I can't find that list"
@@ -24,12 +24,12 @@ function ($location, $routeParams, $scope, CheckList, CheckListItem, Session, Us
     $location.$path('/#/lists');
   }
 
-  $scope.$on('checkList:refresh', function () {
+  $scope.$on('checkList:refresh', function() {
     $scope.checkList = CheckList.get({ id: checkListId });
   });
   $scope.checkList = CheckList.get({ id: checkListId });
 
-  var saveNewCheckListItem = function () {
+  var saveNewCheckListItem = function() {
     var newCheckListItem = new CheckListItem({
       // jshint camelcase: false
       check_list: $scope.checkList.id,
@@ -37,13 +37,13 @@ function ($location, $routeParams, $scope, CheckList, CheckListItem, Session, Us
       description: $scope.checkListItemEdit.description,
       title: $scope.checkListItemEdit.title
     });
-    var successFn = function () {
+    var successFn = function() {
       // jshint camelcase: false
       $scope.checkList.check_list_items.push(newCheckListItem);
       $scope.checkListItemEdit.description = '';
       $scope.checkListItemEdit.title = '';
     };
-    var errorFn = function () {
+    var errorFn = function() {
       $scope.$root.$broadcast('alert', {
         message: "and we can't save " + $scope.checkListItemEdit.title + " right now",
         status: 'danger',
@@ -53,8 +53,8 @@ function ($location, $routeParams, $scope, CheckList, CheckListItem, Session, Us
     newCheckListItem.$save(successFn, errorFn);
   };
 
-  $scope.saveCheckListItem = function () {
-    if (angular.isDefined($scope.checkListItemEdit.id)) {
+  $scope.saveCheckListItem = function() {
+    if(angular.isDefined($scope.checkListItemEdit.id)) {
       var checkListItem = new CheckListItem($scope.checkListItemEdit);
       checkListItem.$update();
     } else {
@@ -62,7 +62,7 @@ function ($location, $routeParams, $scope, CheckList, CheckListItem, Session, Us
     }
   };
 
-  $scope.toggleCheckListItemChecked = function (checkListItem) {
+  $scope.toggleCheckListItemChecked = function(checkListItem) {
     checkListItem.checked = !checkListItem.checked;
     checkListItem = new CheckListItem(checkListItem);
     checkListItem.$update();
