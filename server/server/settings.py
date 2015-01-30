95c618ceb420fa8bb37ca0d385c9d29899ff0a8a
Ben Rabinovitch <github@ben.rabinovitch.ca>
2015-01-29 17:37:43 -0800

    Import DATABASES from prod_settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -21,22 +21,25 @@ try:
 except ImportError:
     pass
 
-DATABASES = {
-    'default': {
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # Or path to database file if using sqlite3.
-        'NAME': 'jotter',
-        # The following settings are not used with sqlite3:
-        'USER': 'jotter',
-        'PASSWORD': 'xoeNgee6',
-        # Empty for localhost through domain sockets
-        # or '127.0.0.1' for localhost through TCP.
-        'HOST': 'localhost',
-        # Set to empty string for default.
-        'PORT': '',
+try:
+    from server.prod_settings import DATABASES
+except ImportError:
+    DATABASES = {
+        'default': {
+            # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # Or path to database file if using sqlite3.
+            'NAME': 'jotter',
+            # The following settings are not used with sqlite3:
+            'USER': 'jotter',
+            'PASSWORD': 'xoeNgee6',
+            # Empty for localhost through domain sockets
+            # or '127.0.0.1' for localhost through TCP.
+            'HOST': 'localhost',
+            # Set to empty string for default.
+            'PORT': '',
+        }
     }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
